71d8f67cd6fa76bdaca7744067fb9e99fdda3061
Luís Gustavo Alves Bezerra <oluisgustavoalves@gmail.com>
2024-08-16 20:02:49 -0300

    Implementação inicial da página de receitas individuais e adição de um novo stylesheet.

---
+++ Individual-Recipes/individual-recipe.php
@@ -0,0 +1,35 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://kit.fontawesome.com/bd4fd62cff.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles/individual-recipe.css">
+    <title>KiDelicia</title>
+</head>
+<body>
+   <main>
+       <article>
+           <div class="recipe-info">
+               <h1 class="recipe-name">Bolo de Cenoura</h1>
+               <p class="recipe-serves">
+                   Serve 5 pessoas / 40 Minutos de Preparo
+               </p>
+               <p>
+                   Autor: Luís Gustavo
+               </p>
+               <div class="socials">
+                   <i class="fa-brands fa-instagram"></i>
+                   <i class="fa-brands fa-twitter"></i>
+                   <i class="fa-brands fa-whatsapp"></i>
+               </div>   
+               <p class="recipe-description">
+                   Um bolo fácil de preparar, de liquidificador, que faz a maior presença. O segredo é a cobertura de glacê toda decorada com flores e casca de laranja. Perfumado, azedinho, bolo de laranja é um clássico que perfuma a casa inteira enquanto assa.
+               </p>
+           </div>
+           <img src="/src/images/img.png" alt="">
+       </article>
+   </main>
+</body>
+</html>
