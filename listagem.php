8fb843a5680917b2cd83e3be223c5372e81302fb
Heloisa Rolins <heloisarolins@gmail.com>
2024-08-17 20:01:14 -0300

    .

--- listagem.php
+++ listagem.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Listagem de Receitas</title>
+  <title>KiDelicia</title>
   <link rel="stylesheet" href="style.css">
   <script src="https://kit.fontawesome.com/ed615418ed.js" crossorigin="anonymous"></script>
   </head>
