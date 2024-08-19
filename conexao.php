bcb5bf4b35678967dd0aa80d4bb646760ad73cc5
dallylademoraes <moraesdallyla6@gmail.com>
2024-08-19 12:26:37 -0300

    reestruturação

---
+++ conexao.php
@@ -0,0 +1,15 @@
+<?php
+$servidor = "localhost";
+$banco = "kidelicia";
+$usuario = "root";
+$senha = "";
+
+$conn = new mysqli($servidor, $usuario, $senha, $banco);
+
+if ($conn->connect_errno) {
+    echo "Conexão falhou: (" . $conn->connect_errno .")" . $conn->connect_errno;
+}
+else
+    echo "Conectado";
+
+?>
